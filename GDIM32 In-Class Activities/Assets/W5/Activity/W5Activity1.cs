13b8f3397e265c8cabe89ec2c50afcb8de3c8ca2
1234321blue <djeremiahyang@gmail.com>
2026-02-03 17:29:52 -0800

    done

--- GDIM32 In-Class Activities/Assets/W5/Activity/W5Activity1.cs
+++ GDIM32 In-Class Activities/Assets/W5/Activity/W5Activity1.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/*
+
 
 public class W5Activity1 : MonoBehaviour
 {
@@ -61,6 +61,10 @@ public class Axe : Item, IBreakable {
 			Break();
 		}
 	}
+	public void Break()
+	{
+		
+	}
 	
 	public override void Use () {
 		Debug.Log("attacking with axe");
@@ -69,30 +73,33 @@ public class Axe : Item, IBreakable {
 }
 
 public class ElvenSword : Item {
-	public void Use () {
+	public override void Use () {
 		Debug.Log("attacking with Elven sword");
 	}
 }
 
 public class Torch : Item, IBreakable {
-	
+	private float _durability = 1;
 	public void Damage (float damage) {
 		_durability -= damage;
 		Debug.Log("torch now has " + _durability + " durability remaining");
 		
 		if(_durability <= 0) {
 			Break();
-		}
+		}}
+		public void Break()
+	{
+		BreakItem();
 	}
+	
 	
 	public void BreakItem () {
 		Debug.Log("torch is broken!");
 	}
 	
-	protected override void Use () {
+	public override void Use () {
 		Debug.Log("lighting area with torch");
 		Damage(1.0f);
 	}
 }
 
-*/
